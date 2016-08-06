8ae466dcb9c3a74fb167aaf00ded42cacd618a9f
Peter Roehlen <peter.roehlen@gmail.com>
2016-08-06 23:33:46 +0700

    Single order now reactive. Partial model cleanup and refactor.

--- public/webapp/model/meteor/mongo/MeteorMongoContextBinding.js
+++ public/webapp/model/meteor/mongo/MeteorMongoContextBinding.js
@@ -1,6 +1,9 @@
 // Provides an abstraction for list bindings
-sap.ui.define(['sap/ui/model/ContextBinding'],
-  function(ContextBinding) {
+sap.ui.define([
+  'sap/ui/model/ContextBinding',
+  'sap/ui/model/ChangeReason'
+],
+  function(ContextBinding, ChangeReason) {
     "use strict";
 
     /**
@@ -21,7 +24,16 @@ sap.ui.define(['sap/ui/model/ContextBinding'],
     var MeteorMongoContextBinding = ContextBinding.extend("meteor-ui5-mongo.MeteorMongoContextBinding", /** @lends meteor-ui5-mongo.MeteorMongoContextBinding.prototype */ {
 
       constructor: function(oModel, sPath, oContext, mParameters, oEvents) {
+        // Call super constructor
         ContextBinding.call(this, oModel, sPath, oContext, mParameters, oEvents);
+
+        // Execute query.  Although, for reasons I don't understand yet, UI5 is
+        // able to get the property values for this context without running a query,
+        // We need to do so so we can observe changes and reactively update the
+        // data in the front end
+        this._runQuery();
+
+        // Don't know what this does but it's needed - copied from ClientModel.js
         var that = this;
         oModel.createBindingContext(sPath, oContext, mParameters, function(oContext) {
           that.bInitial = false;
@@ -29,6 +41,40 @@ sap.ui.define(['sap/ui/model/ContextBinding'],
         });
       }
     });
+
+  MeteorMongoContextBinding.prototype.destroy = function() {
+    // Stop observing changes in any existing query.  Will run forever otherwise.
+    if (this._oQueryHandle) {
+      this._oQueryHandle.stop();
+    }
+  };
+
+  MeteorMongoContextBinding.prototype._runQuery = function() {
+    // Stop observing changes in any existing query.  Will run forever otherwise.
+    if (this._oQueryHandle) {
+      this._oQueryHandle.stop();
+    }
+
+    // Run query
+    const oCursor = this.oModel.runQuery(this.sPath, this.oComponent);
+
+    // Create query handle so we can observe changes
+    this._oQueryHandle = oCursor.observeChanges({
+      addedBefore: (id, fields, before) => {
+        this.fireDataReceived();
+        this._fireChange(ChangeReason.add);
+      },
+
+      changed: (id, fields) => {
+        this._fireChange(ChangeReason.change);
+      },
+
+      removed: (id) => {
+        this._fireChange(ChangeReason.remove);
+      }
+    });
+  }
+
 
     /**
      * @see sap.ui.model.ContextBinding.prototype.refresh
