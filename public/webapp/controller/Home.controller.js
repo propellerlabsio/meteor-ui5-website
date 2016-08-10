a78e29181208621219f2b9cd16341a4e01320a93
Chhunly <chhunly.bin@gmail.com>
2016-08-10 13:48:54 +0700

    Routing four tiles working and Shell working

--- public/webapp/controller/Home.controller.js
+++ public/webapp/controller/Home.controller.js
@@ -1,16 +1,16 @@
 sap.ui.define([
   'sap/ui/core/mvc/Controller',
-	'sap/ui/model/json/JSONModel'
+  'sap/ui/model/json/JSONModel'
 ], function(Controller, JSONModel) {
   "use strict";
 
   var CController = Controller.extend("meteor-ui5.controller.Home", {
 
     onInit: function() {
-
+      this._oRouter = sap.ui.core.UIComponent.getRouterFor(this);
       var oViewModel = new JSONModel({
-		    meteorRelease: Meteor.release.split('@')[1],
-		    ui5Version: sap.ui.version,
+        meteorRelease: Meteor.release.split('@')[1],
+        ui5Version: sap.ui.version,
       });
       this.getView().setModel(oViewModel, "viewModel");
     },
@@ -23,10 +23,27 @@ sap.ui.define([
     onPressGotoMeteor: function() {
       var win = window.open('https://www.meteor.com/', '_blank');
       win.focus();
+
     },
 
-  });
+    onTiledemosPress: function(oEvent) {
+      // Navigate to target route
+      this._oRouter.navTo("demos");
+    },
+    onTiletutorialPress: function(oEvent) {
+      // Navigate to target route
+      this._oRouter.navTo("tutorial");
+    },
+    onTiledocsPress: function(oEvent) {
+      // Navigate to target route
+      this._oRouter.navTo("docs");
+    },
+    onTilerepositoryPress: function(oEvent) {
+      // Navigate to target route
+      this._oRouter.navTo("repository");
+    }
 
+  });
 
   return CController;
 
