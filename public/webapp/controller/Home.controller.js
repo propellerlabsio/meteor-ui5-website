5e447b3679c36dc44f8caa2eead3fb6d6df34643
Chhunly <chhunly.bin@gmail.com>
2016-08-11 17:21:57 +0700

    Refix Unified Shell

--- public/webapp/controller/Home.controller.js
+++ public/webapp/controller/Home.controller.js
@@ -1,13 +1,16 @@
 sap.ui.define([
   'sap/ui/core/mvc/Controller',
+  'sap/ui/core/Fragment',
+  'sap/m/MessageToast',
   'sap/ui/model/json/JSONModel'
-], function(Controller, JSONModel) {
+], function(Controller, Fragment, MessageToast, JSONModel) {
   "use strict";
 
   var CController = Controller.extend("meteor-ui5.controller.Home", {
 
     onInit: function() {
       this._oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
       var oViewModel = new JSONModel({
         meteorRelease: Meteor.release.split('@')[1],
         ui5Version: sap.ui.version,
@@ -41,7 +44,20 @@ sap.ui.define([
     onTilerepositoryPress: function(oEvent) {
       // Navigate to target route
       this._oRouter.navTo("repository");
+    },
+    onhandleUserItemPressed: function() {
+      MessageToast.show("Users temp to press");
+    },
+    onhandlePressConfiguration: function(oEvent) {
+      // MessageToast.show("Text HeaderItem");
+			var oItem = oEvent.getSource();
+			var oShell = this.getView().byId("myShell");
+			var bState = oShell.getShowPane();
+			oShell.setShowPane(!bState);
+			oItem.setShowMarker(!bState);
+			oItem.setSelected(!bState);
     }
+
 
   });
 
