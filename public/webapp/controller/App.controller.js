006d0b33a42fc22f80b640fe3dbedd1def28da9b
Peter Roehlen <peter.roehlen@gmail.com>
2016-08-12 18:43:39 +0700

    Unified shell improvements.  Get home button working.  Add temporary logo.

---
+++ public/webapp/controller/App.controller.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+  'sap/ui/core/mvc/Controller',
+  'sap/m/MessageBox',
+], function(Controller, MessageBox) {
+  "use strict";
+
+  var CController = Controller.extend("meteor-ui5.controller.App", {
+
+    onInit: function() {
+      // Set up route handling
+      this._oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+      this._oRouter.attachRoutePatternMatched(this._onRoutePatternMatched, this);
+    },
+
+
+    _onRoutePatternMatched: function(oEvent) {
+      // Store current route name and view state model
+      this._sRouteName = oEvent.mParameters.name;
+
+      // Show home icon in shell on all routes except "home"
+      this.byId("shellHomeItem").setVisible(this._sRouteName !== "home");
+    },
+
+    handlePressHome: function(){
+        this._oRouter.navTo("home")
+    },
+
+    onhandleUserItemPressed: function(){
+        MessageBox.information("This feature is still under construction.");
+    }
+
+  });
+
+  return CController;
+
+});
