0455bc6d47b97bf86e35537ef44597efd7ff470c
Peter Roehlen <peter.roehlen@gmail.com>
2016-09-02 14:34:24 +0700

    Committing progress but not working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,69 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: ['temp'],
+    copy: {
+      main: {
+        files: [
+          // makes all src relative to cwd
+          {expand: true, cwd: 'public/webapp/', src: ['**'], dest: 'temp/webapp'}
+        ],
+      },
+    },    
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['es2015']
+      },
+      babel_out: {
+        files: [{
+          expand: true,
+          cwd: 'temp/webapp/',
+          src: ['**/*.js'],
+          dest: 'temp/webapp/',
+          // ext: '.js'
+        }]
+      }
+    },    
     openui5_preload: {
       component: {
         options: {
           resources: {
-            cwd: '',
-            prefix: '',
+            cwd: 'temp/webapp',
+            prefix: 'webapp',
             src: [
-              'public/webapp/**/*.js',
-              'public/webapp/**/*.fragment.html',
-              'public/webapp/**/*.fragment.json',
-              'public/webapp/**/*.fragment.xml',
-              'public/webapp/**/*.view.html',
-              'public/webapp/**/*.view.json',
-              'public/webapp/**/*.view.xml',
-              'public/webapp/**/*.properties'
+              '**/*.js',
+              // '**/*.js.map',
+              // '**/*.fragment.html',
+              // '**/*.fragment.json',
+              // '**/*.fragment.xml',
+              // '**/*.view.html',
+              // '**/*.view.json',
+              // '**/*.view.xml',
+              // '**/*.properties'
             ],
-            dest: '.'
+            dest: 'dist'
           },
           compress: true
         },
-        components: true
+        components: 'webapp'
       }
     }
   });
 
+  // Load grunt plugin tasks from pre-installed npm packages
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-openui5');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('build', ['openui5_preload']);
+  // Our build script
+  grunt.registerTask('build', [
+    'clean',          // clear temp folder
+    'copy',          // copy webapp to temp folder 
+    'babel',          // Transpile javascript files in temp overwriting 
+    'openui5_preload' // Build Component-preload.js in webapp from temp
+  ]);
 }
